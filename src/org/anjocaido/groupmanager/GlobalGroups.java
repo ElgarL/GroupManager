4b5d4278b889ea2725f09d29d7ca56fe69e1a8a2
ElgarL <ElgarL@palmergames.com>
2020-06-28 00:48:15 +0100

    Add License information GPLv3

--- src/org/anjocaido/groupmanager/GlobalGroups.java
+++ src/org/anjocaido/groupmanager/GlobalGroups.java
@@ -1,3 +1,20 @@
+/*
+ *  GroupManager - A plug-in for Spigot/Bukkit based Minecraft servers.
+ *  Copyright (C) 2020  ElgarL
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.anjocaido.groupmanager;
 
 import java.io.File;
