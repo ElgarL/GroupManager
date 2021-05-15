e06776d44d7a58ea75ebdbf1b84b65b5511f8f98
ElgarL <ElgarL@palmergames.com>
2021-05-15 11:29:41 +0100

    GMLoggerHandler is still used so no Depreciation.

--- src/org/anjocaido/groupmanager/utils/GMLoggerHandler.java
+++ src/org/anjocaido/groupmanager/utils/GMLoggerHandler.java
@@ -27,7 +27,6 @@ import java.util.logging.LogRecord;
  */
 public class GMLoggerHandler extends ConsoleHandler {
 
-	@Deprecated // Use GroupManager.logger.log(Level level, String msg)
 	@Override
 	public void publish(LogRecord record) {
 		String message = "[GroupManager] " + record.getMessage();
